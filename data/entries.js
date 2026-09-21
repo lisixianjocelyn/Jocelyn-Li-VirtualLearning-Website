24d7588210f107849352d61abceb66854bafb3c2
lisixianjocelyn <lisixianjocelyn@gmail.com>
2026-09-21 09:41:14 +0800

    lskjdf

--- data/entries.js
+++ data/entries.js
@@ -1,6 +1,6 @@
 window.VLD_ENTRIES = [
     {
-      "week": 2,
+      "week": 3,
       "date": "2026-09-15",
       "worked": "I took a practice test with about the same time period as one section of math on the ESAT.",
       "challenge": "The time limit is very intense, causing me not only to speed up the questions so fast but also feel a bit overwhelmed during writing. This is a common question I faced for ESAT, and my coach said I should be active at skipping questions. That's what I am trying this time. I think I can manage more problems but still not be able to finish them.",
